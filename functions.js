315dc0c4e2c42472f8948efc436e22f612604bcb
Aline <alineaparecidacintrasilva@gmail.com>
2021-03-22 13:25:43 -0300

    commit

---
+++ functions.js
@@ -0,0 +1,44 @@
+/* FUNCTIONS */
+
+// Como declarar
+// function calculo(numero) {
+//     return numero * numero;
+// }
+
+// console.log(
+//     calculo(12)
+// );
+
+
+// arrow function
+// const calc = (num, num1) => {
+
+// num = 10;
+// console.log(num);
+//     return num * num1;
+// }
+
+// console.log(calc(null, 55));
+
+
+const numero = 22;
+const calc = (num) => {
+    num = 10;
+    console.log(numero);
+    let valor = 44;
+    console.log(valor);
+        return num + valor;
+    }
+    
+    console.log(calc(null));
+
+
+
+// const materials = [
+//     'Hydrogen',
+//     'Helium',
+//     'Lithium',
+//     'Beryllium'
+//   ];
+  
+//   console.log(materials.map(material => material.length));
